0275046534985a13d0b0343a02f82d8753e11cdd
pussinboots <pussinboots666@googlemail.com>
2013-07-14 03:52:12 +0200

    correct dependencies and name

--- project/packaging.scala
+++ project/packaging.scala
@@ -6,20 +6,20 @@ import com.typesafe.sbt.SbtNativePackager._
 object Packaging {
 
   val settings: Seq[Setting[_]] = packagerSettings ++ deploymentSettings ++ Seq(
-	name in Rpm := "indexer",
+	name in Rpm := "helloworld",
 	version in Rpm := "" + new java.util.Date().getTime,
-	packageSummary in Linux := "Crawler Package",
+	packageSummary in Linux := "Helloworld Package",
 	rpmRelease := "1",
-	rpmVendor := "ingenious",
-	rpmRequirements ++= Seq("chkconfig", "java-1.7.0-openjdk-devel >= 1:1.7", "rabbitmq-server >= 3.0", "epel-release >= 5", "mongo-10gen-server >= 2", "mysql-server >= 5"),
+	rpmVendor := "Frank Ittermann",
+	rpmRequirements ++= Seq("chkconfig", "java-1.7.0-openjdk-devel >= 1:1.7", "apache-tomcat >= 7.0"),
 
-	rpmPost := Option("""chkconfig mongod on
+	/*rpmPost := Option("""chkconfig mongod on
 chkconfig rabbitmq-server on
 chkconfig indexer on
 service mongod start
 service rabbitmq-server start
 service indexer stop
-service indexer start"""),
+service indexer start"""),*/
 
 	rpmLicense := Some("BSD"),
 	linuxPackageMappings <+= (target) map { bd =>
